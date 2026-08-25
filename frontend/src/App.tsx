4a58f073446a5df868489f5c58f0549e90012ce7
Cerberus <146541054+Abhinab04@users.noreply.github.com>
2026-08-26 01:41:55 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import {
+  AuthenticateWithRedirectCallback,
+} from "@clerk/clerk-react";
+
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SigninPage from "./pages/SigninPage/SigninPage";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+
 import { DashboardLayout } from "./components/Layout/DashboardLayout";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import EditorPage from "./pages/EditorPage/EditorPage";
+
 import { Toaster } from "./components/Sonner/sonner";
 
 function App() {
@@ -13,16 +24,51 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signin" element={<SigninPage />} />
 
+          {/* Landing Page */}
+          <Route
+            path="/"
+            element={<LandingPage />}
+          />
+
+          {/* Sign In */}
+          <Route
+            path="/signin"
+            element={<SigninPage />}
+          />
+
+          {/* Clerk SSO Callback */}
+          <Route
+            path="/signin/sso-callback"
+            element={
+              <AuthenticateWithRedirectCallback />
+            }
+          />
+
+          {/* Dashboard Layout */}
           <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/editor" element={<EditorPage />} />
+
+            <Route
+              path="/dashboard"
+              element={<DashboardPage />}
+            />
+
+            <Route
+              path="/editor"
+              element={<EditorPage />}
+            />
+
           </Route>
-          <Route path="*" element={<PageNotFound />} />
+
+          {/* 404 */}
+          <Route
+            path="*"
+            element={<PageNotFound />}
+          />
+
         </Routes>
       </BrowserRouter>
+
       <Toaster />
     </div>
   );
