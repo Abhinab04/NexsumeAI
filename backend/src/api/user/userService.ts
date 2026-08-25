1093a25b166409e30b885972be7938eb07beef3f
Cerberus <146541054+Abhinab04@users.noreply.github.com>
2026-08-25 21:53:16 +0530

    Update userService.ts

--- backend/src/api/user/userService.ts
+++ backend/src/api/user/userService.ts
@@ -1,38 +1,52 @@
-import { UserRepository } from "./userRepository";
-import { ServiceResponse } from "@/common/models/serviceResponse";
-import { IUser } from "./userModel";
-import { logger } from "@/server";
 import { StatusCodes } from "http-status-codes";
+
+import { UserRepository } from "./userRepository.js";
+import { IUser } from "./userModel.js";
+
+import { ServiceResponse } from "../../common/models/serviceResponse.js";
+import { logger } from "../../server.js";
 
 class UserService {
   private userRepository: UserRepository;
 
-  constructor(repository: UserRepository = new UserRepository()) {
+  constructor(
+    repository: UserRepository = new UserRepository(),
+  ) {
     this.userRepository = repository;
   }
 
   async getLoggedinUser(
-    userId: string
+    userId: string,
   ): Promise<ServiceResponse<IUser | null>> {
     try {
-      const user = await this.userRepository.getUserById(userId);
+      const user =
+        await this.userRepository.getUserById(userId);
+
       if (user) {
-        return ServiceResponse.success("Loggedin User", user, StatusCodes.OK);
+        return ServiceResponse.success(
+          "Loggedin User",
+          user,
+          StatusCodes.OK,
+        );
       }
+
       return ServiceResponse.failure(
         "Failed to get Loggedin User",
         null,
-        StatusCodes.BAD_REQUEST
+        StatusCodes.BAD_REQUEST,
       );
     } catch (err) {
-      logger.info(`Error while getting loggedin user, Error: ${err}`);
+      logger.info(
+        `Error while getting loggedin user, Error: ${err}`,
+      );
+
       return ServiceResponse.failure(
         "Failed to get Loggedin User",
         null,
-        StatusCodes.BAD_REQUEST
+        StatusCodes.BAD_REQUEST,
       );
     }
   }
 }
 
-export const userService = new UserService()
+export const userService = new UserService();
