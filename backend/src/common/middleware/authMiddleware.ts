83415bcdf02dcbdc094d627b79c2397f6b9fdba1
Cerberus <146541054+Abhinab04@users.noreply.github.com>
2026-08-25 21:51:40 +0530

    Update authMiddleware.ts

--- backend/src/common/middleware/authMiddleware.ts
+++ backend/src/common/middleware/authMiddleware.ts
@@ -1,39 +1,68 @@
-import { NextFunction, Request, Response, RequestHandler } from "express";
+import type {
+  NextFunction,
+  Request,
+  Response,
+  RequestHandler,
+} from "express";
+
 import jwt from "jsonwebtoken";
-import { env } from "@/common/utils/env";
-import UserModel from "@/api/user/userModel";
-import { logger } from "@/server";
+
+import { env } from "../utils/env.js";
+import UserModel from "../../api/user/userModel.js";
+import { logger } from "../../server.js";
 
 export const verifyJWT: RequestHandler = async (
   req: Request,
   _res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+      req
+        .header("Authorization")
+        ?.replace("Bearer ", "");
 
+    // No token
     if (!token) {
-      logger.info(`Token Not Found`);
+      logger.info("Token Not Found");
+
       req.user = null;
+
       return next();
     }
 
-    const decodedToken = jwt.verify(token, env.ACCESS_TOKEN_SECRET) as jwt.JwtPayload & { _id: string };
-    const user = await UserModel.findById(decodedToken._id);
+    // Verify token
+    const decodedToken = jwt.verify(
+      token,
+      env.ACCESS_TOKEN_SECRET,
+    ) as jwt.JwtPayload & {
+      _id: string;
+    };
+
+    // Find user
+    const user = await UserModel.findById(
+      decodedToken._id,
+    );
 
     if (user) {
       req.user = user;
     } else {
-      logger.info(`Invalid Token, User Not Found`);
+      logger.info(
+        "Invalid Token, User Not Found",
+      );
+
       req.user = null;
     }
 
     return next();
   } catch (err) {
-    logger.info(`Auth middleware Error: ${err}`);
+    logger.info(
+      `Auth middleware Error: ${err}`,
+    );
+
     req.user = null;
+
     return next();
   }
 };
