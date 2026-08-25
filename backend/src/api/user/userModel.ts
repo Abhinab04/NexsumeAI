82a5acda9b649b64055d5d3428a27b52ce85e72e
Cerberus <146541054+Abhinab04@users.noreply.github.com>
2026-08-26 00:52:51 +0530

    Update userModel.ts

--- backend/src/api/user/userModel.ts
+++ backend/src/api/user/userModel.ts
@@ -1,20 +1,66 @@
-import UserModel, { type IUser } from "./userModel.js";
+import jwt, { type SignOptions } from "jsonwebtoken";
+import { Document, model, Schema } from "mongoose";
 
-export class UserRepository {
-  async getUserById(
-    userId: string,
-  ): Promise<IUser | null> {
-    try {
-      const user =
-        await UserModel.findById(userId).exec();
+import { env } from "../../common/utils/env.js";
 
-      return user;
-    } catch (err) {
-      console.error(
-        `Error while getting user by ID: ${err}`,
-      );
-
-      return null;
-    }
-  }
+export interface IUser extends Document {
+  email: string;
+  accessToken?: string | null;
+  refreshToken?: string | null;
 }
+
+export interface UserMethods {
+  generateAccessToken: () => string;
+  generateRefreshToken: () => string;
+}
+
+const userMongooseSchema = new Schema<IUser & UserMethods>(
+  {
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: true,
+    },
+
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userMongooseSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: env.ACCESS_TOKEN_EXPIRY,
+    } as SignOptions,
+  );
+};
+
+userMongooseSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: env.REFRESH_TOKEN_EXPIRY,
+    } as SignOptions,
+  );
+};
+
+const UserModel = model<IUser & UserMethods>(
+  "User",
+  userMongooseSchema,
+);
+
+export default UserModel;
