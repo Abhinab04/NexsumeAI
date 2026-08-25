17399874f03c05a1fc84593600b810b443947b88
Cerberus <146541054+Abhinab04@users.noreply.github.com>
2026-08-26 00:48:00 +0530

    Update index.d.ts

--- backend/src/common/types/express/index.d.ts
+++ backend/src/common/types/express/index.d.ts
@@ -1,4 +1,4 @@
-import { IUser } from "@/api/user/userModel";
+import type { IUser } from "../api/user/userModel.js";
 
 declare global {
   namespace Express {
@@ -7,3 +7,5 @@ declare global {
     }
   }
 }
+
+export {};
