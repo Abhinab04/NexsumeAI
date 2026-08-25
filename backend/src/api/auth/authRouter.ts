78b2451831e5a41515a8fd0eee8ff11c4c82834d
Cerberus <146541054+Abhinab04@users.noreply.github.com>
2026-08-25 18:27:32 +0530

    Update authRouter.ts

--- backend/src/api/auth/authRouter.ts
+++ backend/src/api/auth/authRouter.ts
@@ -1,10 +1,27 @@
 import express, { type Router } from "express";
-import { authController } from "./authController";
-import { verifyJWT } from "@/common/middleware/authMiddleware";
+
+import { authController } from "./authController.js";
+import { verifyJWT } from "../../common/middleware/authMiddleware.js";
 
 export const authRouter: Router = express.Router();
 
-authRouter.post("/login", authController.loginUser);
-authRouter.get("/login/verify/:token", authController.loginWithToken);
-authRouter.post("/login/refresh", authController.refreshAccessToken);
-authRouter.get("/logout", verifyJWT ,authController.logoutUser);
+authRouter.post(
+  "/login",
+  authController.loginUser,
+);
+
+authRouter.get(
+  "/login/verify/:token",
+  authController.loginWithToken,
+);
+
+authRouter.post(
+  "/login/refresh",
+  authController.refreshAccessToken,
+);
+
+authRouter.get(
+  "/logout",
+  verifyJWT,
+  authController.logoutUser,
+);
